82f84444173ce0ed212dbe947f69d67d13be532a
ojijo <ojijotom4@gmail.com>
2025-03-20 14:19:01 +0300

    ojijo

--- training/bin/activate.fish
+++ training/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/emobilis/PycharmProjects/mofacfinal/training
+set -gx VIRTUAL_ENV /home/emobilis/PycharmProjects/SCHOOL/training
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
